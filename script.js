6a33e3a7da188e4be22b24369fac157476d55984
Sean Mena <seanmena13@gmail.com>
2020-08-29 23:56:57 -0600

    added all files

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey = "1b4ec6288b0cc25765135b9928d11892";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+
+
+
+$("#search-btn").on("click", function() {
+    cityName = document.querySelector("#yourCity");
+    console.log(cityName.value);
+
+    const queryURL = apiURL + cityName.value + "&appid=" + apiKey + "&units=imperial"
+    console.log(queryURL)
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+  
+        console.log(queryURL);
+        console.log(response);
+        
+        $("#temp").html("current temp is: " + response.main.temp + "°F");
+
+
+    });
+    
+
+});    
+
+
